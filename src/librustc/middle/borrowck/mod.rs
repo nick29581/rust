0b556f09f8696def5843745cc70c77daa01ec732
Nick Cameron <ncameron@mozilla.com>
2014-06-18 18:43:30 +1200

    Address review comments.

--- src/librustc/middle/borrowck/mod.rs
+++ src/librustc/middle/borrowck/mod.rs
@@ -357,7 +357,7 @@ impl<'a> BorrowckCtxt<'a> {
                     autoderefs: autoderefs, ..}) => {
                 self.mc().cat_expr_autoderefd(expr, autoderefs)
             }
-            _ => {
+            ty::AutoAddEnv(..) | ty::AutoObject(..) => {
                 // no autoderefs
                 self.mc().cat_expr_unadjusted(expr)
             }
