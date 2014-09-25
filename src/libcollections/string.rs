19f35318a50c29d7fb65aa7e52d4cffce5930660
Nick Cameron <ncameron@mozilla.com>
2014-09-26 09:56:35 +1200

    TODO rebasing changes

--- src/libcollections/string.rs
+++ src/libcollections/string.rs
@@ -159,7 +159,7 @@ impl String {
 
         if i > 0 {
             unsafe {
-                res.as_mut_vec().push_all(v.[..i])
+                res.as_mut_vec().push_all(v[..i])
             };
         }
 
@@ -176,7 +176,7 @@ impl String {
             macro_rules! error(() => ({
                 unsafe {
                     if subseqidx != i_ {
-                        res.as_mut_vec().push_all(vv[subseqidx..i_]);
+                        res.as_mut_vec().push_all(v[subseqidx..i_]);
                     }
                     subseqidx = i;
                     res.as_mut_vec().push_all(REPLACEMENT);
